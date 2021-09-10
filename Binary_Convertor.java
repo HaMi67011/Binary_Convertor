3deb4e89f33992266c06009afbc670c38236888d
Ha MI <89655868+HaMi67011@users.noreply.github.com>
2021-09-10 16:25:12 +0500

    Update Binary_Convertor.java

--- Binary_Convertor.java
+++ Binary_Convertor.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 
-public class Converter{
-    public static String toBinary (int num){
-        String binary="";
-    while(num > 0) {
-        binary = (num%2)+binary;
-        num /= 2;
-        }
-        return binary;
-    }
-}
 
-public class Program {
+public class Binary_Convertor {
+
+        public static String toBinary (int num){
+            String binary="";
+        while(num > 0) {
+            binary = (num%2)+binary;
+            num /= 2;
+            }
+            return binary;
+        }
+    
     public static void main(String[ ] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER ");
         int x = sc.nextInt();
-        System.out.print(Converter.toBinary(x));
+        System.out.print(toBinary(x));
     }
+
 }
